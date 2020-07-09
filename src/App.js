035672afcb4aeb19ad705ffcaa9cde0b506161c7
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-07-09 01:11:11 -0300

    pagina 70

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, {useState, Component } from 'react';
 import logo from './logo.svg';
+import TesteComponent from './component/teste'
 import './App.css';
 
+const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+];
+
+
+
+
+var road = { url: "https://mercos.com/uploads/the-road-to-learn-react.pdf#page=", page: 70 }
+
 function App() {
+  const [state,setState] = useState(list);
+  const [name, setName] = useState('');
+  const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App">   
+      <h2>
+        <a href={road.url + road.page} target="_blank">
+          Pagina {road.page}
         </a>
-      </header>
+
+      </h2>
+      <form>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+ 
+      {state.filter(isSearched(name)).map(item => 
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span> / 
+          <span>{item.author}</span> / 
+          <span>{item.num_comments}</span> / 
+          <span>{item.points}</span>
+          <span>
+            <button onClick={() => this.onDismiss(item.objectID)} type="button">
+              Dismiss
+            </button>
+  
+          </span>
+        </div>
+      )}
+
+
+
+
     </div>
   );
 }
-
 export default App;
